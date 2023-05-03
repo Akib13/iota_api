edcc86e786f4af1bf836447aaae40379fc56bb84
nemsqu <nelli.kemi@gmail.com>
2023-05-03 10:27:41 +0200

    Switched to client 2.2.4

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const { Client, initLogger } = require('@iota/client');
+const { ClientBuilder } = require('@iota/client');
 require('dotenv').config({ path: '../.env' });
 
-//initialize logger already here so the program doesn't crash after each request
-initLogger();
 
 //test
 router.get('/data', async function(req, res, next) {
-    if (!process.env.NODE_URL) {
-        throw new Error('.env NODE_URL is undefined, see .env.example');
-    }
 
-    const client = new Client({
-        // Insert your node URL in the .env.
-        nodes: [process.env.NODE_URL],
-        localPow: true,
-    });
+    // client will connect to testnet by default
+    const client = new ClientBuilder()
+        .localPow(true)
+        .build();
+
+    //client.getInfo().then(console.log).catch(console.error);
 
     try {
         const nodeInfo = await client.getInfo();
